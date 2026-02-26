68e7e35192c5241870d3b626567836f7a0a004e4
CrishalLiyanageCS <crishalliyanage.cs825@gmail.com>
2026-02-26 16:57:13 +0530

    Initial commit - University Enrollment System

---
+++ src/main/java/universityenrollmentsystem/UniversityEnrollmentSystem.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package universityenrollmentsystem;
+
+/**** ENTER HERE YOUR DETAILS:*******
+         * 
+         * FIRST NAME: Weliweriya Liyanage Crishal Shanuka Pasan Liyanage
+         *
+         */
+
+public class UniversityEnrollmentSystem {
+
+    public static void main(String[] args) {
+ 
+        EnrollmentManager UoWEnrollment = new WestminsterEnrollmentManager(50);
+        boolean exit = false;
+        while (!exit){
+            exit = UoWEnrollment.runMenu();
+        }
+        
+        System.out.println("System saving and shutting down. Goodbye!");
+    
+    }
+}
